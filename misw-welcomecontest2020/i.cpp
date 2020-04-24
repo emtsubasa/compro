9714fffa1d24543cb7f093c4f7e830a761cbb5d4
emtsubasa <yagimeemeesann@gmail.com>
2020-04-24 17:32:00 +0900

    fix i.cpp

--- misw-welcomecontest2020/i.cpp
+++ misw-welcomecontest2020/i.cpp
@@ -18,16 +18,6 @@ int main() {
 long long solve() {
   long long l = -1, r = 1e16, tmp = 0;
   lim = n * (n + 1) / 2;
-  if (lim % 2) {
-    while (r - l > 1) {
-      long long mid = (l + r) / 2;
-      if (ch(mid, 1))
-        r = mid;
-      else
-        l = mid;
-    }
-    return r;
-  }
   while (r - l > 1) {
     long long mid = (l + r) / 2;
     if (ch(mid, 1))
@@ -36,6 +26,7 @@ long long solve() {
       l = mid;
   }
   tmp = r;
+  if (lim & 1) return tmp;
   l = -1, r = 1e16;
   while (r - l > 1) {
     long long mid = (l + r) / 2;
@@ -50,11 +41,10 @@ long long solve() {
 
 bool ch(long long x, bool flg) {
   long long cnt = 0;
-  for (long long l = 0, r = 0, sum = 0; l < n; r = max(r, l)) {
+  for (long long l = 0, r = 0, sum = 0; l < n; r = max(r, ++l)) {
     while (r < n && sum + a[r] <= x) sum += a[r++];
     cnt += r - l;
-    sum -= a[l++];
+    if (l != r) sum -= a[l];
   }
-  if (lim % 2) return cnt >= (lim + flg) / 2;
   return cnt >= lim / 2 + flg;
 }
