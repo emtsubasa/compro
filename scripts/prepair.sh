d040f167d4130f557de60e3d61aa7045b8f51ce8
emtsubasa <yagimeemeesann@gmail.com>
2020-09-21 18:01:47 +0900

    update README.md

--- scripts/prepair.sh
+++ scripts/prepair.sh
@@ -6,4 +6,4 @@ cd ./$NAME
 echo filenames:
 read FILES
 echo $FILES | xargs mkdir 
-echo $FILES | xargs -n 1 sh -c 'cp ../ghub/kpro/library/preparation.cpp ./$0/$0.cpp && cp ../compile.sh ./$0/compile.sh && code ./$0/$0.cpp'
+echo $FILES | xargs -n 1 sh -c 'cp ../ghub/compro/library/preparation.cpp ./$0/$0.cpp && cp ../compile.sh ./$0/compile.sh && code ./$0/$0.cpp'
