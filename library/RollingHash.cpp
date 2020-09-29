daf891d9685bdcaeb060b24fcadb062477c4c1e8
emtsubasa <yagimeemeesann@gmail.com>
2020-09-29 17:14:20 +0900

    update RollingHash

--- library/RollingHash.cpp
+++ library/RollingHash.cpp
@@ -9,31 +9,30 @@ struct RollingHash {
     Base = base;
     pow.assign(len + 1, 0);
     pow[0] = 1;
-    for (int i = 1; i <= len; ++i) pow[i] = (long long)pow[i - 1] * Base % Mod;
+    for (int i = 1; i <= len; ++i) pow[i] = 1LL * pow[i - 1] * Base % Mod;
   }
-  void add(const string &s) {
+  template <class T>
+  int add(const T &s) {
     int id = hash.size();
-    hash.push_back(vector<int>((int)s.size() + 1, 0));
+    hash.push_back(vector<int>());
     sethash(id, s);
+    return id;
   }
-  void changestr(const int &id, const string &s) {
+  template <class T>
+  void sethash(const int id, const T &s) {
     assert(id < (int)hash.size());
-    sethash(id, s);
-  }
-  void sethash(const int &id, const string &s) {
     int len = s.size();
+    hash[id].resize(len + 1, 0);
     for (int i = 0; i < len; ++i) {
-      hash[id][i + 1] = (long long)hash[id][i] * Base % Mod;
-      hash[id][i + 1] += s[i];
-      if (hash[id][i + 1] >= Mod) hash[id][i + 1] -= Mod;
+      hash[id][i + 1] = 1LL * hash[id][i] * Base % Mod;
+      if ((hash[id][i + 1] += s[i] + 1) >= Mod) hash[id][i + 1] -= Mod;
     }
   }
   // [l,r),0-indexed
-  long long calchash(const int &id, const int &l, const int &r) const {
+  inline int calchash(const int &id, const int &l, const int &r) const {
     assert(r >= l);
-    long long res = hash[id][r];
-    res -= (long long)hash[id][l] * pow[r - l] % Mod;
-    if (res < 0) res += Mod;
+    int res = hash[id][r];
+    res += Mod - 1LL * hash[id][l] * pow[r - l] % Mod;
     if (res >= Mod) res -= Mod;
     return res;
   }
