c91e5f8bf6490af0bd04b9671bd4ae0e1b15c61b
emtsubasa <yagimeemeesann@gmail.com>
2020-12-07 23:12:51 +0900

    update Matrix

--- library/Matrix.cpp
+++ library/Matrix.cpp
@@ -54,6 +54,40 @@ struct Matrix {
   Matrix operator-(const Matrix &B) const { return (Matrix(*this) -= B); }
   Matrix operator*(const Matrix &B) const { return (Matrix(*this) *= B); }
   Matrix operator^(const long long k) const { return (Matrix(*this) ^= k); }
+
+  Matrix trans() {
+    size_t m = height(), n = width();
+    Matrix res(n, m);
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < m; ++j) res[i][j] = (*this)[j][i];
+    return res;
+  }
+
+  Matrix inv() {
+    assert(height() == width());
+    size_t n = height();
+    Matrix B(n, 2 * n);
+    for (int i = 0; i < n; ++i) {
+      B[i][i + n] = 1;
+      for (int j = 0; j < n; ++j) B[i][j] = (*this)[i][j];
+    }
+    for (int i = 0; i < n; ++i) {
+      int piv = i;
+      for (int j = i; j < n; ++j)
+        if (abs(B[j][i]) > abs(B[piv][i])) piv = j;
+      // not exist or unique
+      assert(abs(B[piv][i]) >= 0);
+      swap(B[i], B[piv]);
+      for (int j = i + 1; j < 2 * n; ++j) B[i][j] /= B[i][i];
+      for (int j = 0; j < n; ++j)
+        if (i != j)
+          for (int k = i + 1; k < 2 * n; ++k) B[j][k] -= B[j][i] * B[i][k];
+    }
+    Matrix res(n);
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < n; ++j) res[i][j] = B[i][j + n];
+    return res;
+  }
   friend ostream &operator<<(ostream &os, Matrix &p) {
     size_t m = p.height(), n = p.width();
     for (int i = 0; i < m; i++) {
