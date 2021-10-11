8f871cee5cc6ceeefaf151c9a9bb085a8eed14c9
emtsubasa <yagimeemeesann@gmail.com>
2021-10-11 15:24:56 +0900

    update script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,5 +1,5 @@
 echo filename:
 read NAME
-g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack
-# g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack -fsanitize=address -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
+# g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack
+g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack -fsanitize=address
 oj t -t 3 --diff
--- scripts/onlycompile.sh
+++ scripts/onlycompile.sh
@@ -1,4 +1,4 @@
 echo filename:
 read NAME
-g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack
-# g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack -fsanitize=address -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
+# g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack
+g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack -fsanitize=address
