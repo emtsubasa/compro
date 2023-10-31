bf4f5117485b94689087fe9e5db1711d530177ca
emtsubasa <yagimeemeesann@gmail.com>
2023-11-01 08:38:24 +0900

    fix prepair.sh

--- tools/prepair.sh
+++ tools/prepair.sh
@@ -6,4 +6,4 @@ cd ./$NAME
 echo filenames:
 read FILES
 echo $FILES | xargs mkdir 
-echo $FILES | xargs -n 1 sh -c 'cp ../ghub/compro/tools/preparation.cpp ./$0/$0.cpp && cp ../compile.sh ./$0/compile.sh && code ./$0/$0.cpp'
+echo $FILES | xargs -n 1 sh -c 'cp ../preparation.cpp ./$0/$0.cpp && cp ../compile.sh ./$0/ && cp ../onlycompile.sh ./$0/ && code ./$0/$0.cpp'
