c4c33ca3c8460616921cdbc2264c22641cd9f7d7
emtsubasa <yagimeemeesann@gmail.com>
2023-11-01 22:26:37 +0900

    update setting

--- tools/compile.sh
+++ tools/compile.sh
@@ -1,5 +1,4 @@
 echo filename:
 read NAME
-# g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack
-g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack -fsanitize=address
+g++ -std=c++17 $NAME.cpp -O2 -I .
 oj t -t 3 --diff
--- tools/onlycompile.sh
+++ tools/onlycompile.sh
@@ -1,4 +1,3 @@
 echo filename:
 read NAME
-# g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack
-g++ -std=c++17 $NAME.cpp -O2 -I . -fsplit-stack -fsanitize=address
+g++ -std=c++17 $NAME.cpp -O2 -I .
